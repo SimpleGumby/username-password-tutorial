69afb149fa9fb893811c75ad4198c1bba4638d9a
taylor <taylormoran882@gmail.com>
2024-11-18 14:23:23 -0800

    auth file added

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 
+var authRouter = require('./routes/auth');
+
 var app = express();
 
 app.locals.pluralize = require('pluralize');
@@ -24,13 +26,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
+app.use('/', authRouter);
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
